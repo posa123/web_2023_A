6542c807c1509409c4930b6f6d50dd14bc3f1c2a
504 <504@DESKTOP-SI28KUP>
2023-07-13 12:24:34 +0900

    0713

---
+++ backend/src/java1/day01/Ex2_타입.java
@@ -0,0 +1,71 @@
+package java1.day01; // 현재 클래스 패키지경로
+
+public class Ex2_타입 { // class s
+	public static void main(String[] args) { // main s
+		// 1. [ p.41 ] 진수를 표현하는 방법
+			// 진수 : 데이터 표현하는 수의 형식 // 기계어[2진수/이진코드] 표현하는데 한계 극복
+			// 0,1 : 2진수 , 0~7 : 8진수 , 0~9 : 10 진수 , 0~9ABCDEF : 16진수
+			
+		int var1 = 0b1011; // 데이터 앞에 0b 
+			System.out.println("var1 : " +var1);
+		int var2 = 0206;   // 데이터 앞에 0; 206[8] -> 136 [10]
+			System.out.println("var2 : " +var2);
+		int var3 = 365;
+			System.out.println("var3 : " +var3);
+		int var4 = 0xa; // (16진수) 데이터 앞에 0x
+			System.out.println("var4 : " +var4);
+		
+		// [ p.42 ] byte 타입 [+-128]
+			byte b1 = -128; System.out.println("b1 : " + b1);
+			byte b2 = 127; System.out.println("b2 : " +b2);
+			//byte b3 = 200; System.out.println("b3 : " +b3); // 오류 : Type mismatch 허용 범위 벗어남
+			
+			// short 타입
+			short s1 = 3200; System.out.println("s1 : " +s1);
+			//short s2 = -50000; System.out.println("s2 : " +s2); // 오류 : Type mismatch 허용 범위 벗어남
+			
+			//int 타입 [ 대략 +-21억정도 ] * 정수 기본타입 : 1.우리 코드로 작성한 정수
+			int i1 = 2000000000; System.out.println("i1 : " +i1);
+			//int i2 = 3000000000; System.out.println("i2 : " +i2);
+			
+			// long 타입 [ 대락 +- 21억이상 자동 ]
+			long l1 = 10;	System.out.println("l1 : " +l1);
+			// long l2 = 3000000000; 	// 직접 작성한 정수 30억은 INT타입 -> 기본/자동
+			long l3 = 3000000000L; // 직접 작성한 정수를 long 타입 변경 -> 정수 뒤에 L 붙이기
+					System.out.println("l3 : " +l3);
+	} // main e
+} // class e
+
+/*
+ 	타입/자료형 : 데이터 저장할때 사용되는 메모리 크기
+ 		1. 메모리 효율성 높일수 있다.
+ 		2. 가독성 높음
+ 		
+ 					메모리크기		허용범위
+ 	 	논리타입
+ 	 	문자타입
+ 	 	문자열타입
+ 	 	정수형타입
+ 	 		byte	1바이트		[ -128 ~ 127 ]		대략 +-100	
+ 	 		short   2바이트		[-32,768 ~ 32,767 ] 대략 +-3만정도
+ 	 		int		4바이트		[ 대략 +- 21억 정도 ] 
+ 	 		long	8바이트		[ 대략 +- 21억 이상]
+ 	 	실수타입
+ 	 	
+ 	ex)
+ 		선물상자 = 변수
+ 		사탕 = 데이터
+ 		500상자 엄마꺼 = 지갑선물
+ 		500상자 아빠꺼 = 시계선물
+ 	 	
+ 	 	// 용량단위 : 
+ 	 	  	0,1 -> 1bit 	-> 01010101 -> 8자리 -> 8bit -> 1byte
+ 	 	  	1byte -> 1024byte -> 1Kbyte
+ 	 	  	1kb -> 1024kb -> 1mb
+ 	 	  	1mb -> 1024mb -> 1gb
+ 	 	
+ 		
+  
+  
+ 
+*/
---
+++ backend/src/java1/day01/Hello.java
@@ -0,0 +1,19 @@
+// 한줄 주석
+/* 여러줄 주석 */
+
+
+package java1.day01; // 패키지 경로/폴더 경로
+
+public class Hello { // class start 클래스 선언 //'Hello' 클래스명 = 숫자시작x,공백x,첫글자대문자
+	// 1. JAVA 코드 가장 단위 = 클래스 = 모든 코드는 클래스 안에서 작성
+	// 2. 모든 실행코드는 main함수 안에서 작성 [ 스레드=코드읽은흐름 ] 포함
+	
+	// { } 마다 tab 1번 적용
+	public static void main(String[] args) { // m start / 메소드=함수 선언
+		
+		// syso + 컨트롤+스페이스바	엔터!!
+		System.out.println("Hello, Java");
+		// ctrl + F11 , 해당 오른쪽클릭-> RUN , 상단메뉴 RUN
+	} // m end
+
+}// class end
