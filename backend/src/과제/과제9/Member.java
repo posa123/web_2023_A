4bccd6be5544e780946e5e62c899fe71e1e03c8d
504 <504@DESKTOP-SI28KUP>
2023-07-25 17:35:26 +0900

    0725

---
+++ backend/src/과제/과제9/Member.java
@@ -0,0 +1,25 @@
+package 과제.과제9;
+
+// 목적 : 설계도, 객체의모델
+public class Member {
+	
+	// 1. 필드 : 객체의 데이터 를 저장하는곳
+	String id;	 
+	String password;
+	String name;
+	String phone;
+	int age;
+	
+	// 2. 생성자
+	
+	public Member( String id , String password , String name , String phone , int age) {
+		this.id = id;
+		this.password=password;
+		this.name=name;
+		this.phone=phone;
+		this.age=age;
+	} 	// 1. 매개변수 5개 정의한 생성자
+
+	// 3. 메소드
+	
+}
